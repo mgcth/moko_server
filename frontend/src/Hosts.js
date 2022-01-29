c0d76ae494f67f923608fad38443c62b4cd60306
Mladen Gibanica <mgcth@users.noreply.github.com>
2022-01-29 19:47:56 +0100

    Add hosts file

---
+++ frontend/src/Hosts.js
@@ -0,0 +1,18 @@
+const host = "http://moko:5000"
+const host_ws = "ws://moko:5000"
+
+const host_read_camera = "/read-camera"
+const host_delete_camera = "/delete-camera"
+const host_stream = "/stream"
+const host_camera_config = "/camera-config"
+const host_save_camera = "/save-camera"
+
+export {
+    host,
+    host_ws,
+    host_read_camera,
+    host_delete_camera,
+    host_stream,
+    host_camera_config,
+    host_save_camera
+};
