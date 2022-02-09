599e7b9a5478e6b225a3e80b07e1ce617a47c946
Mladen Gibanica <mgcth@users.noreply.github.com>
2022-02-09 17:48:49 +0100

    Change order of camera modes

--- backend/camera_settings.py
+++ backend/camera_settings.py
@@ -21,16 +21,16 @@ class CameraSettings:
 
         self.modes = {
             "V1": [
-                CameraHardware(0, (1920, 1080), (1, 30), True, False, "Partial"),
                 CameraHardware(1, (2492, 1944), (0.1666, 15), True, True, "Full"),
+                CameraHardware(0, (1920, 1080), (1, 30), True, False, "Partial"),
                 CameraHardware(2, (1296, 972), (1, 42), True, False, "Full"),
                 CameraHardware(3, (1296, 730), (1, 49), True, False, "Full"),
                 CameraHardware(4, (640, 480), (42.1, 90), True, False, "Full")
             ],
             "V2": [
-                CameraHardware(0, (1920, 1080), (0.1, 30), True, False, "Partial"),
                 CameraHardware(1, (3280, 2464), (0.1, 15), True, True, "Full"),
                 CameraHardware(2, (1640, 1232), (0.1, 40), True, False, "Full"),
+                CameraHardware(0, (1920, 1080), (0.1, 30), True, False, "Partial"),
                 CameraHardware(3, (1640, 922), (0.1, 40), True, False, "Full"),
                 CameraHardware(4, (1280, 720), (40, 90), True, False, "Partial"),
                 CameraHardware(5, (640, 480), (40, 90), True, False, "Partial")
