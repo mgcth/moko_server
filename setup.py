e1a790b761d2cb8160a91b6df68da08125bd30d8
Mladen Gibanica <mgcth@users.noreply.github.com>
2022-03-22 18:26:28 +0100

    Fix setup.py file and add platofrm check, update github workflow too

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
-
 from setuptools import setup
+import platform
 
-setup(
-    name="moko_server",
-    install_requires=[
-        "aiofiles"=="0.8.0",
-        "httptools"=="0.3.0",
-        "multidict"=="5.2.0",
-        "numpy"=="1.21.4",
-        "picamera"=="1.13",
-        "PyYAML"=="6.0",
-        "sanic"=="21.12.1",
-        "Sanic-Cors"=="2.0.0",
-        "sanic-routing"=="0.7.2",
-        "ujson"=="5.1.0",
-        "uvloop"=="0.16.0",
-        "websockets"=="10.1"
+base = (
+    [
+        "aiofiles==0.8.0",
+        "httptools==0.3.0",
+        "multidict==5.2.0",
+        "numpy==1.21.4",
+        "PyYAML==6.0",
+        "sanic==21.12.1",
+        "Sanic-Cors==2.0.0",
+        "sanic-routing==0.7.2",
+        "ujson==5.1.0",
+        "uvloop==0.16.0",
+        "websockets==10.1",
     ],
 )
+
+base = (
+    base + "picamera==1.13"
+    if platform.system == "Linux" and platform.machine == "armv7l"
+    else base
+)
+
+setup(name="moko_server", install_requires=base)
