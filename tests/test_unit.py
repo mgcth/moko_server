5bd4c883d65d423f2e8d378efa6f2a7e995007de
Mladen Gibanica <mgcth@users.noreply.github.com>
2022-03-22 18:33:11 +0100

    Finish user unit tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -5,11 +5,15 @@ username = "username"
 password = "pass"
 
 
-def test_unit_user_init():
-    """Test user initialisation."""
+def test_unit_user():
+    """Test user class, initialisation and methods."""
 
     user = User(user_id, username, password)
 
     assert user.user_id == user_id
     assert user.username == username
     assert user.password == password
+
+    assert repr(user) == "User(id='{0}')".format(user_id)
+
+    assert user.to_dict() == {"user_id": user_id, "username": username}
