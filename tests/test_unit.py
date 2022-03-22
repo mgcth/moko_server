95aeb9127e28ae1cc5217ed1fc090176c4d08e17
Mladen Gibanica <mgcth@users.noreply.github.com>
2022-03-22 18:26:52 +0100

    Add unit test for user init method

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,15 @@
+from moko_server.user import User
+
+user_id = 0
+username = "username"
+password = "pass"
+
 
 def test_unit_user_init():
-    """Test user initialisation.
+    """Test user initialisation."""
 
-    """
-    pass
+    user = User(user_id, username, password)
+
+    assert user.user_id == user_id
+    assert user.username == username
+    assert user.password == password
