2f9da36c520dde3354daa2ba838c28cbeb72c650
Mladen Gibanica <mgcth@users.noreply.github.com>
2022-03-24 18:09:25 +0100

    Fix docstrings in user file

--- src/moko_server/user.py
+++ src/moko_server/user.py
@@ -1,23 +1,24 @@
 class User:
     """
+    User class.
     """
 
     def __init__(self, id, username, password):
         """
+        Construction user object.
         """
-
         self.user_id = id
         self.username = username
         self.password = password
 
     def __repr__(self):
         """
+        Represent object as string.
         """
-
         return "User(id='{0}')".format(self.user_id)
 
     def to_dict(self):
         """
+        Create dict from object.
         """
-
         return {"user_id": self.user_id, "username": self.username}
