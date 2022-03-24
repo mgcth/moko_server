90a4d859a7c65558ee7055f50d70bf1a37e08b09
Mladen Gibanica <mgcth@users.noreply.github.com>
2022-03-24 18:10:21 +0100

    Fix flake8 in camera_settings

--- src/moko_server/camera_settings.py
+++ src/moko_server/camera_settings.py
@@ -1,17 +1,18 @@
 from dataclasses import dataclass
 from collections import namedtuple
 
-CameraHardware = namedtuple("CameraHardware", ["Id", "Resolution", "Framerates", "Video", "Image", "FoV"])
+CameraHardware = namedtuple(
+    "CameraHardware", ["Id", "Resolution", "Framerates", "Video", "Image", "FoV"]
+)
+
 
 @dataclass(frozen=True)
 class CameraSettings:
     """
     Raspberry Pi Camera settings class.
     """
-    module = {
-        "ov5647": "V1",
-        "imx219": "V2"
-    }
+
+    module = {"ov5647": "V1", "imx219": "V2"}
 
     modes = {
         "V1": [
@@ -19,7 +20,7 @@ class CameraSettings:
             CameraHardware(1, (1920, 1080), (1, 30), True, False, "Partial"),
             CameraHardware(2, (1296, 972), (1, 42), True, False, "Full"),
             CameraHardware(3, (1296, 730), (1, 49), True, False, "Full"),
-            CameraHardware(4, (640, 480), (42.1, 90), True, False, "Full")
+            CameraHardware(4, (640, 480), (42.1, 90), True, False, "Full"),
         ],
         "V2": [
             CameraHardware(0, (3280, 2464), (0.1, 15), True, True, "Full"),
@@ -27,6 +28,6 @@ class CameraSettings:
             CameraHardware(2, (1920, 1080), (0.1, 30), True, False, "Partial"),
             CameraHardware(3, (1640, 922), (0.1, 40), True, False, "Full"),
             CameraHardware(4, (1280, 720), (40, 90), True, False, "Partial"),
-            CameraHardware(5, (640, 480), (40, 90), True, False, "Partial")
-        ]
+            CameraHardware(5, (640, 480), (40, 90), True, False, "Partial"),
+        ],
     }
